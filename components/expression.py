06a1bf3cd1584c4b9bb23a01236c47a0b910328a
JustLeahy <124506610+JustLeahy@users.noreply.github.com>
2021-03-30 11:51:18 +0000

    update readme example

--- components/expression.py
+++ components/expression.py
@@ -15,3 +15,47 @@ Sample Usage:
     print(SgExpression.EvaluateExpression(table, u"a * (b - a_b)"))
     print(SgExpression.EvaluateExpression(table, u"MIN(a * (b - a_b))"))
     print(SgExpression.EvaluateExpression(table, u"MAX(a * (b - a_b))"))
+    print(SgExpression.EvaluateExpression(table, u"-7 + a*(b-a_b)"))
+    print(SgExpression.EvaluateExpression(table, u"max(a*(b-a_b))"))
+    print("---")
+    print(SgExpression.EvaluateExpression(table, u"a * b - a_b + a_b % a"))
+    print(SgExpression.EvaluateExpression(table, u"MIN(a * b - a_b + a_b % a)"))
+    print(SgExpression.EvaluateExpression(table, u"MAX(a * b - a_b + a_b % a)"))
+    print(SgExpression.EvaluateExpression(table, u"a*b-a_b+a_b%a"))
+    print(SgExpression.EvaluateExpression(table, u"max(a*b-a_b+a_b%a)"))
+    print("---")
+    print(SgExpression.EvaluateExpression(table, u"3 + 2 = 5"))
+    print(SgExpression.EvaluateExpression(table, u"6 = 3 + 2"))
+    print(SgExpression.EvaluateExpression(table, u"a_b * a_b - b > 10"))
+    print(SgExpression.EvaluateExpression(table, u"\"aaa\" is \"aaa\""))
+    print(SgExpression.EvaluateExpression(table, u"c like \"%\""))
+    print(SgExpression.EvaluateExpression(table, u"c like \"%A\""))
+    print(SgExpression.EvaluateExpression(table, u"c like \"C_C\""))
+    print(SgExpression.EvaluateExpression(table, u"c like \"A\""))
+    print(SgExpression.EvaluateExpression(table, "\"%%%\" like \"\\%_\\%\""))
+    print(SgExpression.EvaluateExpression(table, "\"a%%\" like \"\\%_\\%\""))
+    print(SgExpression.EvaluateExpression(table, "\"%a%\" like \"\\%_\\%\""))
+    print(SgExpression.EvaluateExpression(table, u"c regexp \"B*\""))
+    print(SgExpression.EvaluateExpression(table, u"c regexp \"[B-C]*\""))
+    print(SgExpression.EvaluateExpression(table, u"\"BB\" in (\"A\", \"B\", c)"))
+    print("---")
+    print(SgExpression.EvaluateExpression(table, "not a + 2 >= b"))
+    print(SgExpression.EvaluateExpression(table, "not a + 2 >= b and a_b > 10"))
+    print(SgExpression.EvaluateExpression(table, "b < 5 || a_b > 10"))
+    print("---")
+    print(SgExpression.EvaluateExpression(table, "sum(a + b)"))
+    print(SgExpression.EvaluateExpression(table, "avg(a * a)"))
+    print(SgExpression.EvaluateExpression(table, u"CONCAT(\"a\", c, \"ccc\", -7 + 8)"))
+"""
+
+import re
+import regex  # need recursive pattern
+
+import definition as df
+import utilities as util
+import table as tb
+import math
+import datetime
+
+class SgExpression:
+    """A set of utility functions to evaluate expressions."""
