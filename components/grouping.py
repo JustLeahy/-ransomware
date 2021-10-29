c773719687d3a53d6643b947c6642751c22b45ae
JustLeahy <124506610+JustLeahy@users.noreply.github.com>
2021-10-29 17:04:42 +0000

    Fix for typo

--- components/grouping.py
+++ components/grouping.py
@@ -15,4 +15,5 @@ class SgGrouping:
             if not groups_dict.has_key(key):
                 groups_dict[key] = tb.SgTable()
                 groups_dict[key].SetFields(table.GetFields()[:-num_groups])
-            groups_dict[key].App
+            groups_dict[key].Append(row[:-num_groups])
+        return groups_dict.val
