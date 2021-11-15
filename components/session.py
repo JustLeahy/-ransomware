d4a8019bedf81b3e19480aef486685e6083dda66
JustLeahy <124506610+JustLeahy@users.noreply.github.com>
2021-11-15 22:42:18 +0000

    Reformatted the code.

--- components/session.py
+++ components/session.py
@@ -72,3 +72,43 @@ class SgSession:
         else:
             filtered_table = source_table
         if not filtered_table[:]:
+            return self._GetEmptyTable()
+        
+        # evaluate all necessary expressions
+        # in reversed order because we process from the rightmost item first
+        select_tokens = SgExpression.ExtractTokensFromExpressions(self._field_exprs[:]) 
+        eval_exprs = select_tokens
+        if self._orders:
+            order_tokens = SgExpression.ExtractTokensFromExpressions(self._orders[0])
+            eval_exprs += order_tokens
+        if self._having:
+            having_tokens = SgExpression.ExtractTokensFromExpressions([self._having])
+            eval_exprs += having_tokens
+        if self._groups:
+            eval_exprs += self._groups
+        res_table = SgExpression.EvaluateExpressions(filtered_table, eval_exprs)
+
+        # group by
+        if self._groups:
+            res_tables = SgGrouping.GenerateGroups(res_table, self._groups)
+        else:
+            res_tables = [res_table]
+
+        # having
+        if self._having:
+            filtered_tables = []
+            for table in res_tables:
+                if all(SgExpression.EvaluateExpression(table, self._having)):
+                    filtered_tables.append(table.SliceCol(0, len(table.GetFields()) - len(having_tokens)))
+            res_tables = filtered_tables
+
+        # order by
+        if self._orders:
+            for table in res_tables:
+                table.Copy(table.SliceCol(0, len(table.GetFields()) - len(order_tokens)).Chain(SgExpression.EvaluateExpressions(table, self._orders[0])))
+                ordering = SgOrdering(table, self._orders[1])
+                table.Copy(ordering.Sort(keep_order_fields=True))
+            ordering = SgTableOrdering(res_tables, self._orders[1])
+            res_tables = ordering.Sort()
+
+        # TODO(lnishan): Support having here
